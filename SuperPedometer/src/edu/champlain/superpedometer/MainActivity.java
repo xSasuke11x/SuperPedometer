c3d949b812fa0cd43e912be1b003ce5a497320db
Kennedy Tran <kennedy.tran@mymail.champlain.edu>
2013-10-20 01:29:49 -0400

    Submit button now directs to Pedometer.java. Toggle button now accurately toggles on touch.

--- SuperPedometer/src/edu/champlain/superpedometer/MainActivity.java
+++ SuperPedometer/src/edu/champlain/superpedometer/MainActivity.java
@@ -3,17 +3,18 @@ package edu.champlain.superpedometer;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
-import android.content.res.Configuration;
+//import android.content.pm.ActivityInfo;
+//import android.content.res.Configuration;
 import android.view.Menu;
 import android.view.View;
-import android.widget.CompoundButton;
+/*import android.widget.CompoundButton;
 import android.widget.ToggleButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.content.Intent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;*/
 
 
 public class MainActivity extends Activity {
@@ -22,50 +23,12 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        ToggleButton toggle = (ToggleButton)findViewById(R.id.toggleButton);
-        if ( getRequestedOrientation() != ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED ) {
-            toggle.setChecked(true);
-        } else {
-            toggle.setChecked(false);
-        }
-        toggle.setOnCheckedChangeListener(listener);
-        
-        Button b1 = (Button) findViewById(R.id.submit);
-
-        // Setup the listeners for the buttons, and the button handler      
-        b1.setOnClickListener(buttonhandler);
-    }        
+    }
     
-    View.OnClickListener buttonhandler = new View.OnClickListener() { 
-
-    	// Now I need to determine which button was clicked, and which intent or activity to launch.         
-    	public void onClick(View v) {
-    		Intent myIntent1 = new Intent(MainActivity.this, Pedometer.class);
-    		MainActivity.this.startActivity(myIntent1);
-    	} 
-    };
-    
-    OnCheckedChangeListener listener = new OnCheckedChangeListener() {
-        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-            int current = getResources().getConfiguration().orientation;
-            if (isChecked) {
-                switch(current) {
-                case Configuration.ORIENTATION_LANDSCAPE:
-                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-                    break;
-                case Configuration.ORIENTATION_PORTRAIT:
-                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-                    break;
-                default:
-                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
-                }
-            } else {
-                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
-            }
-        }
-    };
-
+    public void onButtonClick(View view) {
+        Intent intent = new Intent(MainActivity.this, Pedometer.class);
+        startActivity(intent);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- SuperPedometer/src/edu/champlain/superpedometer/Pedometer.java
+++ SuperPedometer/src/edu/champlain/superpedometer/Pedometer.java
@@ -2,6 +2,10 @@ package edu.champlain.superpedometer;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class Pedometer extends Activity {
 	
@@ -10,5 +14,13 @@ public class Pedometer extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pedometer);
 
+        final ToggleButton toggle = (ToggleButton) findViewById(R.id.toggleButton);
+    	toggle.setOnClickListener(new OnClickListener() {
+    		public void onClick(View v) {
+    			Toast.makeText(getBaseContext(),
+    			"Pedometer is "+toggle.getText().toString(),
+   				Toast.LENGTH_LONG).show();
+   	        }
+   	    });
 	}
 }
